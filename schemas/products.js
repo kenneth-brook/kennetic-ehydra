e71a337c3f1d24c6d17aaec7666efb5249466750
Kenneth Brook <kenneth.brook@hotmail.com>
2023-01-21 23:42:15 -0500

    product schema started

---
+++ schemas/products.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'price',
+      type: 'number',
+      title: 'Price',
+    },
+  ],
+}
