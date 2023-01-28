17a786394c668e5b5e94b52a3218de41f8b9ae93
Kenneth Brook <kenneth.brook@hotmail.com>
2023-01-28 11:58:55 -0500

    vendor table added

--- schemas/index.js
+++ schemas/index.js
@@ -3,5 +3,6 @@ import category from './category'
 import richText from './richText'
 import store from './store'
 import images from './images'
+import vendor from './vendor'
 
-export const schemaTypes = [products, category, richText, store, images]
+export const schemaTypes = [products, category, richText, store, images, vendor]
---
+++ schemas/vendor.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'vendor',
+  type: 'document',
+  title: 'Vendor',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'website',
+      type: 'url',
+      title: 'Website',
+    },
+    {
+      name: 'address',
+      type: 'string',
+      title: 'Address',
+    },
+    {
+      name: 'phone',
+      type: 'string',
+      title: 'Phone',
+    },
+    {
+      name: 'discountCodes',
+      type: 'string',
+      title: 'Discount Codes',
+    },
+  ],
+}
