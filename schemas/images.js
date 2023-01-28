60d8493f21124f040f98ad21070c6d06e0e3804e
Kenneth Brook <kenneth.brook@hotmail.com>
2023-01-27 20:02:35 -0500

    updated image loader

--- schemas/images.js
+++ schemas/images.js
@@ -5,13 +5,9 @@ export default {
   fields: [
     {
       name: 'image',
-      type: 'image',
       title: 'Image',
-    },
-    {
-      name: 'alt',
-      type: 'string',
-      title: 'Alt Text',
+      type: 'array',
+      of: [{type: 'image'}],
     },
   ],
 }
